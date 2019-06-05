9ca62916688c7e87d1f1478d078d66b77d9c2b1b
kegisa <vit.levin2011@yandex.ru>
2019-06-05 21:20:19 +0400

    Updates

---
+++ precache-manifest.dee6581216a27d6d90c67bf3f965040b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5678948f8858b0289c1c",
+    "url": "/vkApps/static/js/runtime~main.6a05e7d9.js"
+  },
+  {
+    "revision": "667b021babf350c5759a",
+    "url": "/vkApps/static/js/main.899de4fd.chunk.js"
+  },
+  {
+    "revision": "9271b2df4e98da8da016",
+    "url": "/vkApps/static/js/2.8620120c.chunk.js"
+  },
+  {
+    "revision": "9271b2df4e98da8da016",
+    "url": "/vkApps/static/css/2.afbc27b8.chunk.css"
+  },
+  {
+    "revision": "efa423cc02fe23cd6f9919c24d635a02",
+    "url": "/vkApps/index.html"
+  }
+];
