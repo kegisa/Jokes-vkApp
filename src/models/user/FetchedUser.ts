27b3f0235b4e5b7cb3e50f4fc06a1c9ba4833d65
aprivalov <artem.privalov@waveaccess.ru>
2019-06-21 12:53:27 +0400

    almost working migrated to TS solution.

---
+++ src/models/user/FetchedUser.ts
@@ -0,0 +1,12 @@
+export interface FetchedUser {
+    id: any;
+    photo_200?: any;
+    city?: City;
+    first_name?: string;
+    last_name?: string;
+}
+
+
+export interface City {
+    title: string,
+}
