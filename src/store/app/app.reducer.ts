d9db37381e7dc7e881bc8469efbf2b088487bfa7
aprivalov <artem.privalov@waveaccess.ru>
2019-06-23 16:21:16 +0400

    current version of app

--- src/store/app/app.reducer.ts
+++ src/store/app/app.reducer.ts
@@ -1,17 +1,33 @@
 import * as appActions from '@store/app/app.actions';
+import {FetchedUser} from '@models';
 
 export interface AppState {
+    isFetching: boolean;
+    fetchedUser: Partial<FetchedUser>;
 }
 
-const initialState: AppState = {};
+const initialState: AppState = {
+    isFetching: false,
+    fetchedUser: {},
+};
 
 export const appReducer = (
     state = initialState,
     action: appActions.Actions
 ): AppState => {
-    // switch (action.type) {
-    //     default:
-    //         return state;
-    // }
-    return state;
+    switch (action.type) {
+        case appActions.START_FETCHING_USER_INFO:
+            return {
+                ...state,
+                isFetching: true,
+            };
+        case appActions.FINISH_FETCHING_USER_INFO:
+            return {
+                ...state,
+                isFetching: false,
+                fetchedUser: action.payload,
+            };
+        default:
+            return state;
+    }
 };
