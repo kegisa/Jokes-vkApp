eeb05bb83c0770b8cb9f3e9a106cadabb8ebb164
kegisa <vit.levin2011@yandex.ru>
2019-06-23 10:31:16 +0400

    init

---
+++ src/index.tsx
@@ -0,0 +1,52 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import connect from '@vkontakte/vkui-connect';
+import './assets/styles/main.scss';
+import '@vkontakte/vkui/dist/vkui.css';
+
+const rootElement = document.getElementById('root');
+
+connect.subscribe((e) => {
+    switch (e.detail.type) {
+        case 'VKWebAppUpdateConfig':
+            const schemeAttribute = document.createAttribute('scheme');
+            schemeAttribute.value = e.detail.data.scheme ? e.detail.data.scheme : 'client_light';
+            document.body.attributes.setNamedItem(schemeAttribute);
+            break;
+        default:
+        // console.log(e.detail.type);
+    }
+});
+
+connect.send('VKWebAppInit', {});
+
+let render = () => {
+    const AppContainer = require('./containers/App').App;
+    ReactDOM.render(
+        <AppContainer/>,
+        rootElement
+    );
+};
+
+render();
+
+if (module.hot) {
+    const renderApp = render;
+    const renderError = (error: any) => {
+        const RedBox = require('redbox-react');
+        ReactDOM.unmountComponentAtNode(rootElement as Element);
+        ReactDOM.render(<RedBox error={error}/>, rootElement);
+    };
+
+    render = () => {
+        try {
+            renderApp();
+        } catch (error) {
+            renderError(error);
+        }
+    };
+
+    module.hot.accept('./containers/App', () => {
+        setTimeout(render);
+    });
+}
---
+++ src/store/app/app.actions.ts
@@ -0,0 +1,10 @@
+import {ActionsUnion} from '@store/actions-helpers';
+
+export const Actions = {
+};
+
+export const Thunks = {
+};
+
+export type Actions = ActionsUnion<typeof Actions>;
+export type Thunks = ActionsUnion<typeof Thunks>;
