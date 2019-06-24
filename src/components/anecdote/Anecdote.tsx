63f8431c35624abc4956fcb3b9f232805947e879
kegisa <vit.levin2011@yandex.ru>
2019-06-24 23:53:41 +0400

    fucking error

--- src/components/anecdote/Anecdote.tsx
+++ src/components/anecdote/Anecdote.tsx
@@ -2,18 +2,19 @@ import React, {FunctionComponent} from 'react';
 import {Button, Cell, Group, List} from '@vkontakte/vkui';
 import Icon16Like from '@vkontakte/icons/dist/16/like';
 import Icon16LikeOutline from '@vkontakte/icons/dist/16/like_outline';
+import Icon16ReplyOutline from '@vkontakte/icons/dist/16/reply_outline';
 import {IAnecdote} from '@models';
 
 interface Props {
     id: number;
     joke: IAnecdote;
     likePressed: any;
+    repostPressed: any;
 }
 
 const AnecdoteComponent: FunctionComponent<Props> = props => {
-    const {id, joke, likePressed} = props;
+    const {id, joke, likePressed, repostPressed} = props;
     const isLikePressed = joke.like === 1;
-    // tslint:disable-next-line:no-console
     return (
         <Group className="post" key={joke.anek_id}>
             <List>
@@ -40,6 +41,15 @@ const AnecdoteComponent: FunctionComponent<Props> = props => {
                         >
                             {joke.likes}
                         </Button>
+                    }
+                    before={
+                        <Button
+                            className="likes"
+                            level="tertiary"
+                            after={<Icon16ReplyOutline/>}
+                            size="xl"
+                            // onClick={() => repostPressed(joke.joke)}
+                        />
                     }
                 />
             </List>
--- src/shared/GlobalConsts.ts
+++ src/shared/GlobalConsts.ts
@@ -1,1 +1,2 @@
 export const API_URL = 'https://66160595b12e.sn.mynetname.net/';
+export const SIGNATURE = 'Больше анекдотов здесь -> https://vk.com/app7009416';
