63681951dc7418878381fb68e8432c3cb838b541
kegisa <vit.levin2011@yandex.ru>
2019-06-30 22:44:58 +0400

    add link to server

--- src/shared/GlobalConsts.ts
+++ src/shared/GlobalConsts.ts
@@ -1,2 +1,2 @@
-export const API_URL = 'https://66160595b12e.sn.mynetname.net/';
+export const API_URL = 'https://simpleabraham.work:444/';
 export const SIGNATURE = 'Больше анекдотов здесь -> https://vk.com/app7009416';
